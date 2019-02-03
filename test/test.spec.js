57100f9f02b1bce0011f5d087fbc3a9c01e9e3cb
Mike Woodward <michaelcwoodward@gmail.com>
2019-02-02 23:06:45 -0600

    karma and travis

---
+++ test/test.spec.js
@@ -0,0 +1,18 @@
+describe("test", function() {
+    it("should work", function () {
+      expect(true).toEqual(true);
+    });
+  });
+
+
+  describe("test1", function() {
+    it("should work", function () {
+      expect(true).toEqual(true);
+    });
+  });
+
+  describe("test2", function() {
+    it("should work", function () {
+      expect(true).toEqual(true);
+    });
+  });
